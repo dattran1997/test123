ff4ed2c2c363951170621adaee903649c16d960c
dattran1997 <dattran1997@gmail.com>
2019-05-12 02:00:46 +0700

    new

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from models.database import mobile_collection
+from bson.objectid import ObjectId
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    allphone = mobile_collection.find()
+    return render_template('index.html',allphone = allphone)
+
+@app.route('/products')
+def products():
+    allphone = mobile_collection.find()
+    return render_template('products.html',allphone = allphone)
+
+@app.route('/products/single/<id>')
+def single(id):
+    detail_mobile = mobile_collection.find_one({"_id" : ObjectId(id)})
+    return render_template('single.html',detail_mobile=detail_mobile)
+
+if __name__ == '__main__':
+ app.run(debug=True)
